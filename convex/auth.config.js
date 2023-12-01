00c016d75db4374741bb3a3f49f4bb92d2cde933
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-12-01 15:57:25 +0100

    added convex and clerk to authenticate

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+/* trunk-ignore-all(eslint/import/no-anonymous-default-export) */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://relaxing-ferret-63.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
