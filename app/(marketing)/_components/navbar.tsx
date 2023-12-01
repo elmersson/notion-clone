e482f2426c9b6a312acb5e2aab891bb56cbeb7dd
elmersson <elmerssonrasmus@gmail.com>
2023-12-01 15:58:20 +0100

    added convex and clerk to authenticate (#4)
    Co-authored-by: RasmusElmersson <rasmus.elmersson@martinservera.se>

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,14 +2,19 @@
 
 import Link from "next/link";
 
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 import { Logo } from "./logo";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -21,6 +26,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Lotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Lotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
