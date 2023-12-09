7fb365eb42fafa44ab5654be7ee98dd3e6eb4162
RasmusElmersson <rasmus.elmersson@martinservera.se>
2023-12-09 11:25:26 +0100

    feat: settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+
 import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             storageKey="lotion-theme"
           >
             <Toaster position="bottom-center" />
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
