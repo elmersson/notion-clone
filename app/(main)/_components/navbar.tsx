257a826f5772016171e4ec47d71355d465474b37
elmersson <elmerssonrasmus@gmail.com>
2023-12-11 17:29:30 +0100

    feat: preview-mode (#22)
    Co-authored-by: RasmusElmersson <rasmus.elmersson@martinservera.se>

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { Id } from "@/convex/_generated/dataModel";
 
 import { Banner } from "./banner";
 import { Menu } from "./menu";
+import { Publish } from "./publish";
 import { Title } from "./title";
 
 interface NavbarProps {
@@ -45,6 +46,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         <div className="flex w-full items-center justify-between">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
